80d1293b8cd33efffa1c87ac584501aac88ccc5b
Kevin Olesker <k.olesker@gmail.com>
2019-09-01 21:58:00 -0300

    Created top posts feature module to handle top posts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { TopPostsComponent } from './top-posts/components/top-posts/top-posts.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: TopPostsComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/top-posts/top-posts.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+
+import { TopPostsComponent } from './components/top-posts/top-posts.component';
+
+
+@NgModule({
+  imports: [],
+  declarations: [
+    TopPostsComponent,
+  ],
+})
+export class TopPostsModule {
+  constructor() {}
+}
