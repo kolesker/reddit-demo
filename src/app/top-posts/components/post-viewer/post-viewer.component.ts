1692fe70df8a8213cc741176fbb5ad166f73b22e
Kevin Olesker <k.olesker@gmail.com>
2019-09-01 22:05:51 -0300

    Created post and post-viewer component for splitted screen

---
+++ src/app/top-posts/components/post-viewer/post-viewer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-post-viewer',
+  templateUrl: 'post-viewer.component.html',
+  styleUrls: ['./post-viewer.component.scss']
+})
+
+export class PostViewerComponent {
+  constructor() { }
+}
--- src/app/top-posts/top-posts.module.ts
+++ src/app/top-posts/top-posts.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 
 import { TopPostsComponent } from './components/top-posts/top-posts.component';
+import { PostComponent } from './components/post/post.component';
+import { PostViewerComponent } from './components/post-viewer/post-viewer.component';
 
 
 @NgModule({
   imports: [],
   declarations: [
+    PostComponent,
+    PostViewerComponent,
     TopPostsComponent,
   ],
 })
